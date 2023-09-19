a5c8c52299451d8ca107778a75c7cdfd8048f540
Patricio de Villa <patricio.devilla@prysmex.com>
2023-09-19 01:06:30 -0600

    update client version

--- lib/kibana/dashboard.rb
+++ lib/kibana/dashboard.rb
@@ -1,7 +1,7 @@
 require 'securerandom'
 
 module Kibana
-  CLIENT_VERSION = "8.9.1"
+  CLIENT_VERSION = "8.10.1"
   module Dashboard
     # PANELS_JSON_VISUALIZATION_VERSION = '8.9.1'.freeze
     CORE_MIGRATION_VERSION = '8.8.0'.freeze
