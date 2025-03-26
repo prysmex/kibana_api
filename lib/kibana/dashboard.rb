2ae3365cc3764feb0e26d889a4d2a692294b210d
Patricio de Villa <patricio.devilla@prysmex.com>
2025-03-26 00:44:33 -0600

    kibana 8.17.4

--- lib/kibana/dashboard.rb
+++ lib/kibana/dashboard.rb
@@ -3,7 +3,7 @@
 require 'securerandom'
 
 module Kibana
-  CLIENT_VERSION = '8.17.0'
+  CLIENT_VERSION = '8.17.4'
   module Dashboard
 
     # @note This could be removed if classes inherit from ActiveSupport::HashWithIndifferentAccess
