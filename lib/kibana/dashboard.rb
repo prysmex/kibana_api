2e4673d9ae18e8e53a8c1dbc6b91ea0f6cf0f492
Patricio de Villa <patricio.devilla@prysmex.com>
2026-07-27 17:44:40 -0600

    kibana 9.4.4

--- lib/kibana/dashboard.rb
+++ lib/kibana/dashboard.rb
@@ -3,7 +3,7 @@
 require 'securerandom'
 
 module Kibana
-  CLIENT_VERSION = '9.4.3'
+  CLIENT_VERSION = '9.4.4'
   module Dashboard
 
     # @note This could be removed if classes inherit from ActiveSupport::HashWithIndifferentAccess
